532ff8a67c729624163552a3e66f48bbfb84f695
amirmy <yamirmohammad@gmail.com>
2016-04-13 17:34:56 +0430

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -236,7 +236,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {139983390},--Sudo users
+    sudo_users = {139983390,184425385},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[sickmind bot 2.3
